0ef41c28bc7167a431618868b5ce36cc2f16aada
maticzav <matic.zavadlal@gmail.com>
2020-12-28 17:20:07 +0100

    :bug: fix vercel deployment

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -1,19 +1,10 @@
-import { ApolloServer } from 'apollo-server-micro'
+import { ApolloServer } from '@saeris/apollo-server-vercel'
 import { schema } from '../src/'
 
 const server = new ApolloServer({
   schema,
+  playground: true,
+  introspection: true,
 })
 
-const handler = server.createHandler({
-  path: '/api/graphql',
-})
-
-// dont parse the body, next. apollo got this
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
-
-export default handler
+export default server.createHandler()
