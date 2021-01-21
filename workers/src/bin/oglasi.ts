8cb19d1ffd824f53aca1ab1c78e802e284712473
maticzav <matic.zavadlal@gmail.com>
2021-01-21 17:54:12 +0100

    fix: batch oglasi to improve time

--- workers/src/bin/oglasi.ts
+++ workers/src/bin/oglasi.ts
@@ -2,83 +2,124 @@ import { PrismaClient } from '@prisma/client'
 
 import { withPrisma } from '../prisma'
 import { naloziOglas } from '../scarpper'
+import { sum } from '../utils'
 
 /* Configuration */
+
+const chunk_size = 30
+
+/**
+ * Med izvajanjem računamo še povprečen čas za shranjevanje podatkov.
+ */
+let benchmarks: number[] = []
 
 /* Program */
 
 async function main(client: PrismaClient) {
-  // Naloži informacije o oglasih.
-  const zaznamki = await client.zaznamek.findMany({
-    where: {
-      aktiven: true,
-      posredovanje: {
-        in: ['ODDAJA', 'PRODAJA'],
+  let page = 0
+
+  /**
+   * Listaj po straneh dokler so še oglasi.
+   */
+
+  while (true) {
+    // Benchmark
+    const start = Date.now()
+
+    // Naloži informacije o oglasih.
+    const zaznamki = await client.zaznamek.findMany({
+      // Filter
+      where: {
+        aktiven: true,
+        posredovanje: {
+          in: ['ODDAJA', 'PRODAJA'],
+        },
       },
-    },
-    orderBy: {
-      updated_at: 'desc',
-    },
-    include: {
-      oglasi: {
-        orderBy: { created_at: 'desc' },
-      },
-    },
-  })
-
-  const st_zaznamkov = zaznamki.length
-
-  console.log(`Našel ${st_zaznamkov} aktivnih zaznamkov...`)
-
-  for (const [index, zaznamek] of zaznamki.entries()) {
-    const oglas = await naloziOglas(zaznamek.url)
-
-    if (oglas === null) continue
-
-    // Poglej že obstoječ oglas in primerjaj podatke.
-
-    if (zaznamek && zaznamek.oglasi.length > 0) {
-      const zadnji_oglas = zaznamek.oglasi[0]
-
-      // Če se cena od zadnjega oglasa ni spremenila ne naredi novega zaznamka.
-      if (oglas.cena === zadnji_oglas.cena) {
-        console.log(`(${index}/${st_zaznamkov}) Nespremenjen ${zaznamek.id}`)
-        continue
-      }
-    }
-
-    // Izračunaj meritve
-    let cena_na_m2: number | undefined
-    if (oglas.velikost && oglas.cena) {
-      cena_na_m2 = oglas.cena / oglas.velikost
-    }
-
-    // Shrani oglas v bazo.
-    const db = await client.oglas.create({
-      data: {
-        //   Podatki
-        zaznamek: { connect: { db_id: zaznamek.db_id } },
-        // Informacije
-        naslov: oglas.naslov,
-        kratek_opis: oglas.kratek_opis,
-        dolg_opis: oglas.daljsi_opis,
-        slike: { set: oglas.slike },
-        // Lokacija
-        vrsta: oglas.vrsta,
-        regija: oglas.regija,
-        upravna_enota: oglas.enota,
-        obcina: oglas.obcina,
-        // Meritve
-        velikost: oglas.velikost,
-        cena: oglas.cena,
-        leto: oglas.leto,
-        st_sob: oglas.st_sob,
-        // Analiza
-        cena_na_m2: cena_na_m2,
+      // Pagination
+      orderBy: { id: 'asc' },
+      take: chunk_size,
+      skip: page * chunk_size,
+      // Selection
+      include: {
+        oglasi: {
+          orderBy: { created_at: 'desc' },
+        },
       },
     })
 
-    console.log(`(${index}/${st_zaznamkov}) Oglas ${db.zaznamek_id}...`)
+    if (zaznamki.length === 0) break
+
+    /**
+     * Najdi oglase in si zabeleži spremembe.
+     */
+
+    await Promise.all(
+      zaznamki.map(async (zaznamek) => {
+        // Naloži oglas.
+        const oglas = await naloziOglas(zaznamek.url)
+
+        if (oglas === null) return
+
+        // Poglej že obstoječ oglas in primerjaj podatke.
+        if (zaznamek && zaznamek.oglasi.length > 0) {
+          const zadnji_oglas = zaznamek.oglasi[0]
+
+          // Če se cena od zadnjega oglasa ni spremenila ne naredi novega zaznamka.
+          if (oglas.cena === zadnji_oglas.cena) {
+            console.log(`Nespremenjen ${zaznamek.id}`)
+            return
+          }
+        }
+
+        // Izračunaj meritve
+        let cena_na_m2: number | undefined
+        if (oglas.velikost && oglas.cena) {
+          cena_na_m2 = oglas.cena / oglas.velikost
+        }
+
+        // Shrani oglas v bazo.
+        const db = await client.oglas.create({
+          data: {
+            //   Podatki
+            zaznamek: { connect: { db_id: zaznamek.db_id } },
+            // Informacije
+            naslov: oglas.naslov,
+            kratek_opis: oglas.kratek_opis,
+            dolg_opis: oglas.daljsi_opis,
+            slike: { set: oglas.slike },
+            // Lokacija
+            vrsta: oglas.vrsta,
+            regija: oglas.regija,
+            upravna_enota: oglas.enota,
+            obcina: oglas.obcina,
+            // Meritve
+            velikost: oglas.velikost,
+            cena: oglas.cena,
+            leto: oglas.leto,
+            st_sob: oglas.st_sob,
+            // Analiza
+            cena_na_m2: cena_na_m2,
+          },
+        })
+
+        console.log(`Oglas ${db.zaznamek_id}...`)
+      }),
+    )
+
+    // Benchmark taske.
+    const end = Date.now()
+    const delta = (end - start) / chunk_size
+    benchmarks.push(delta)
+
+    // Logs
+
+    const chg = Math.floor(delta)
+    const avg = Math.floor(sum(benchmarks) / benchmarks.length)
+
+    console.log(`page(${page}): ${chg}/${avg} ms`)
+
+    // Paginate
+    page += 1
   }
 }
 
